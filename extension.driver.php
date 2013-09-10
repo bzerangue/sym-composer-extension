d88fa29aaed4c4055c22b9c51355854d7f3b5636
Brian Zerangue <brian.zerangue@gmail.com>
2013-09-10 15:40:33 -0500

    Initial Commit

---
+++ extension.driver.php
@@ -0,0 +1,11 @@
+<?php
+	/*
+		This file is here to make sure that 
+		Symphony plays nice with Composer
+		since Composer creates a folder called
+		composer in the extensions folder.
+	*/
+
+	Class extension_composer extends Extension {
+
+	}
